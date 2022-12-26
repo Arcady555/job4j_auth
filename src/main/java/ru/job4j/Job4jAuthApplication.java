2bc864658340507f3de8f80c04826e3e10bec6ff
Arcady555 <parfenov7233@gmail.com>
2022-12-26 09:08:49 +0300

    RESTFull. Описание архитектуры [#6884 #374726]

--- src/main/java/ru/job4j/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -2,18 +2,8 @@ package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import javax.sql.DataSource;
-
-//@Configuration
-//@ComponentScan(basePackages = "ru.job4j")
 @SpringBootApplication
-//@EntityScan("ru.job4j.domain")
 public class Job4jAuthApplication {
 
 	public static void main(String[] args) {

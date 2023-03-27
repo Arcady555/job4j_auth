a21120e66149d681b3a0a388e2ce0e2cd2e13d7d
Arcady555 <parfenov7233@gmail.com>
2023-03-27 15:30:28 +0300

    правки для docker-3-3

--- src/test/java/ru/job4j/Job4jAuthApplicationTests.java
+++ src/test/java/ru/job4j/Job4jAuthApplicationTests.java
@@ -2,9 +2,8 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@SpringBootTest
 class Job4jAuthApplicationTests {
 
 	@Test
